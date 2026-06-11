c97e33b5d116ac0346513fe43286bac1c32ddbee
Nurbu <lamanurbu2@gmail.com>
2026-06-11 13:59:35 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/sneakerdrops/SneakerDrops9Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.sneakerdrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerDrops9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerDrops9Application.class, args);
+	}
+
+}
